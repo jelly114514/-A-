a2f0422841144b9d5587bec8ecf42cec64d8a4e3
Jelly <108531120+jelly114514@users.noreply.github.com>
2024-01-16 02:19:39 +0800

    1_dataset.py
    Datasets实战代码

--- Jelly今天学了什么/Pytorch_learning/Dataset类/1_dataset.py
+++ Jelly今天学了什么/Pytorch_learning/Dataset类/1_dataset.py
@@ -1,9 +1,9 @@
 from torch.utils.data import Dataset
 from PIL import Image
 import os
-
+#自己定义的类 
 class My_dataset(Dataset):
-
+    #初始化
     def __init__(self, root_dir, label_dir) -> None:
         self.root_dir = root_dir
         self.label_dir = label_dir
@@ -21,13 +21,36 @@ class My_dataset(Dataset):
     def __len__(self):
         return len(self.image_path)
 
+#获取数据集
 root_dir = "Pytorch_learning\\Datasets\\train"
 ants_label_dir = "ants_image"
 ants_dataset = My_dataset(root_dir, ants_label_dir)
 bees_label_dir = "bees_image"
 bees_dataset = My_dataset(root_dir, bees_label_dir)
-
 train_dataset = ants_dataset + bees_dataset
+
+#写标签
+root_dir = 'Datasets/train'
+target_dir = 'ants_image'
+img_path = os.listdir(os.path.join(root_dir, target_dir))
+label = target_dir.split('_')[0]
+out_dir = 'ants_label'
+for i in img_path:
+    file_name = i.split('.jpg')[0]
+    with open(os.path.join(root_dir, out_dir,"{}.txt".format(file_name)),'w') as f:
+        f.write(label)
+#写标签
+root_dir = 'Datasets/train'
+target_dir = 'bees_image'
+img_path = os.listdir(os.path.join(root_dir, target_dir))
+label = target_dir.split('_')[0]
+out_dir = 'bees_label'
+for i in img_path:
+    file_name = i.split('.jpg')[0]
+    with open(os.path.join(root_dir, out_dir,"{}.txt".format(file_name)),'w') as f:
+        f.write(label)
+
+#测试
 print(len(bees_dataset))
 print(len(ants_dataset))
 print(len(train_dataset))
