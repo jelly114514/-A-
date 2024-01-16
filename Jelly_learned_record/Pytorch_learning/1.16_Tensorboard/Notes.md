0364463dc6557d2937a7c45dfde5841bef64f3e8
Jelly <108531120+jelly114514@users.noreply.github.com>
2024-01-16 16:48:27 +0800

    更新

---
+++ Jelly_learned_record/Pytorch_learning/1.16_Tensorboard/Notes.md
@@ -0,0 +1,8 @@
+# Attention
+
+**label(图像标题记得换)**
+
+**writer.add_scalar("label",y, x)是先y后x**
+
+**__路径一定不可以含中文和空格__**
+
---
+++ Jelly_learned_record/Pytorch_learning/1.16_Tensorboard/test_tb.py
@@ -0,0 +1,9 @@
+from torch.utils.tensorboard import SummaryWriter
+writer = SummaryWriter("logs")
+
+writer.add_image()
+
+for i in range(100):
+    writer.add_scalar("y = 2x", 2*i, i)
+
+writer.close()
